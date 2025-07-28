efede5bcd284215436cd5726fb9d80a7c573b4b7
SHIVRAJPATIL123 <118585118+SHIVRAJPATIL123@users.noreply.github.com>
2025-07-28 14:37:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,3 +17,26 @@ variable "resource_group_name" {
   description = "Name of the existing Azure Resource Group"
   type        = string
 }
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Azure Client ID (Service Principal App ID)"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure Client Secret"
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure Tenant ID"
+  sensitive   = true
+
+}
