34ec88376745cceb3f0b22c332ff7cb065ca25c2
SHIVRAJPATIL123 <118585118+SHIVRAJPATIL123@users.noreply.github.com>
2025-07-28 10:42:56 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${var.cluster_name}"
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "${var.cluster_name}-dns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
