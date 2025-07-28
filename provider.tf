af51eef414c0030dd72d668a4c7365832f0dda87
SHIVRAJPATIL123 <118585118+SHIVRAJPATIL123@users.noreply.github.com>
2025-07-28 11:53:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.84.0"  # Choose the version you want or latest 3.x
+    }
+  }
+
+  required_version = ">= 1.6.0"
+}
+
 provider "azurerm" {
   features {}
 }
+
