999ecf340d7403c3d5c450c002804c06920844e9
SHIVRAJPATIL123 <118585118+SHIVRAJPATIL123@users.noreply.github.com>
2025-07-28 16:53:07 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.37.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name   = "Az-104"
+    storage_account_name  = "glowsatf"
+    container_name        = "sacont"
+    key                   = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
